5babaa7c292311b631833a7b5c67ded687c14570
mattszarek13 <mszarek@ceiamerica.com>
2021-12-28 04:53:44 -0600

    m4

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id;
+
+        [Required()]
+        [MaxLength(50)]
+        public string Description;
+
+
+    }
+}
--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using WishList.Data;
+
 namespace WishList
 {
     public class Startup
@@ -18,6 +20,10 @@ namespace WishList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<ApplicationDbContext>( options => {
+                options.UseInMemoryDatabase("WishList");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
