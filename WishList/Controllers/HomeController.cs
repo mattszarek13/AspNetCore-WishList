6e08500a5291cec61409c9e738804c05f43c7881
mattszarek13 <mszarek@ceiamerica.com>
2021-12-28 04:39:42 -0600

    m3

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View("Index");
+    }
+
+    public IActionResult Error()
+    {
+        return View("Error");
+    }
+}
