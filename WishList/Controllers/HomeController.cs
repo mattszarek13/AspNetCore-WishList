1ba1c375051c5519fbbcd1bf4fa61caecb62b5f4
mattszarek13 <mszarek@ceiamerica.com>
2021-12-28 04:41:15 -0600

    m3

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -1,14 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 
-public class HomeController : Controller
+namespace WishList.Controlers
 {
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-        return View("Index");
-    }
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
 
-    public IActionResult Error()
-    {
-        return View("Error");
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
     }
 }
